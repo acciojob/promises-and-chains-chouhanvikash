3020fa98618bbb10b9b1a9a0a96e4bc202bc1df5
chouhanvikash <96488608+chouhanvikash@users.noreply.github.com>
2025-09-19 05:55:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-//your JS code here. If required.
+let age = document.getElementById("age");
+let name = document.getElementById("name");
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", fun);
+
+function fun(e) {
+  e.preventDefault();
+
+  if (name.value === "" || age.value === "") {
+    alert("Please enter valid details.");
+    return;
+  }
+
+  setTimeout(promiseF, 4000);
+
+  function promiseF() {
+    const promise = new Promise((resolve, reject) => {
+      if (Number(age.value) > 18) {
+        resolve(`Welcome, ${name.value}. You can vote.`);
+      } else {
+        reject(`Oh sorry ${name.value}. You aren't old enough.`);
+      }
+    });
+
+    promise
+      .then(msg => alert(msg))
+      .catch(err => alert(err));
+  }
+}
